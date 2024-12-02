31b0f8ca561cb2d9df0f3fc7af398518eb3945fb
Kamilo1205 <kamilo_1205@outlook.com>
2024-12-02 11:06:42 -0500

    feat: :sparkles: agregar iconos de react-icons

---
+++ src/components/ui/icons.tsx
@@ -0,0 +1,10 @@
+import { LuEye, LuEyeOff } from 'react-icons/lu';
+import { ImSpinner8 } from 'react-icons/im';
+import { FcGoogle } from 'react-icons/fc';
+
+export const Icons = {
+  spinner: ImSpinner8,
+  google: FcGoogle,
+  eye: LuEye,       
+  eyeOff: LuEyeOff,
+}
